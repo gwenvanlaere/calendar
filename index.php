eb23a9a231dc2b3bbbb9d6c597c70cc72d41dd16
GwenVanLaere <gwen_van_laere@hotmail.com>
2021-11-07 20:00:10 +0100

    link fix 554

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- @format -->
+<?php
+echo '<pre>';
+print_r(dirname($_SERVER['SCRIPT_NAME']));
+echo '</pre>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><a href="<?php echo dirname($_SERVER['SCRIPT_NAME']);?>/Views/index.php">Link</a></h1>
+</body>
+
+</html>
